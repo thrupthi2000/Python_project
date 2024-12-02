cc640cf870d8fcd4e42274613a20cfb74c63128b
thrupthi2000 <thrupthi2103@gmail.com>
2024-12-02 10:45:47 +0530

    this is python file

---
+++ main.py
@@ -0,0 +1,15 @@
+# # main.py
+# import requests
+
+# def fetch_data():
+#     response = requests.get("https://api.github.com")
+#     print(response.json())
+
+# if __name__ == "__main__":
+#     fetch_data()
+
+
+# Hello, World! program in Python
+print("Hello, World!")
+print("Iam Thrupthi")
+
